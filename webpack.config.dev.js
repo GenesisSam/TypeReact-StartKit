628f0636524df1b91ffea2a3065dabd20067dccf
GenesisSam <donghyun.lee@vingle.net>
2018-03-22 11:02:41 +0900

    Add devSever allow hosts

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,4 +2,14 @@ const merge = require("webpack-merge");
 const common = require("./webpack.config.common.js");
 const webpack = require("webpack");
 
-module.exports = merge(common, { mode: "development" });
+module.exports = merge(common, {
+  mode: "development",
+  devServer: {
+    allowedHosts: [".lvh.me", "localhost"],
+    compress: true,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    historyApiFallback: true,
+  },
+});
