817fb887bbe173b7a01eb92561ab0b8732b5648e
GenesisSam <samuel940401@gmail.com>
2019-09-21 00:07:36 +0900

    Fix router push

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
 import { ThemeProvider } from "styled-components";
 import { createBrowserHistory } from "history";
-import { ConnectedRouter } from "connected-react-router";
+import { ConnectedRouter, routerMiddleware } from "connected-react-router";
 
 import theme from "common/theme";
 
@@ -17,35 +17,35 @@ import rootReducer from "./rootReducer";
 import getMessages from "common/intl/getMessages";
 
 class ClientRender {
-	public render() {
-		const currentLocale = navigator.language.split("-")[0];
-		const messages = getMessages(currentLocale);
+  public render() {
+    const currentLocale = navigator.language.split("-")[0];
+    const messages = getMessages(currentLocale);
 
-		const history = createBrowserHistory();
-		const middleWares: any = [thunk];
+    const history = createBrowserHistory();
+    const middleWares: any = [thunk, routerMiddleware(history)];
 
-		if (process.env.NODE_ENV === "development") {
-			middleWares.push(createLogger());
-		}
+    if (process.env.NODE_ENV === "development") {
+      middleWares.push(createLogger());
+    }
 
-		const store = createStore(rootReducer(history), applyMiddleware(...middleWares));
+    const store = createStore(rootReducer(history), applyMiddleware(...middleWares));
 
-		ReactDOM.render(
-			<IntlProvider locale={navigator.language} messages={messages}>
-				<ConnectedRouter history={history}>
-					<Provider store={store}>
-						<ThemeProvider theme={theme}>
-							<Switch>
-								<Route exact path="/hello" component={() => <div>H3llo World!</div>} />
-								<Route exact component={Hello} />
-							</Switch>
-						</ThemeProvider>
-					</Provider>
-				</ConnectedRouter>
-			</IntlProvider>,
-			document.getElementById("mainContainer"),
-		);
-	}
+    ReactDOM.render(
+      <IntlProvider locale={navigator.language} messages={messages}>
+        <ConnectedRouter history={history}>
+          <Provider store={store}>
+            <ThemeProvider theme={theme}>
+              <Switch>
+                <Route exact path="/hello" component={() => <div>H3llo World!</div>} />
+                <Route exact component={Hello} />
+              </Switch>
+            </ThemeProvider>
+          </Provider>
+        </ConnectedRouter>
+      </IntlProvider>,
+      document.getElementById("mainContainer")
+    );
+  }
 }
 
 const render = new ClientRender();
