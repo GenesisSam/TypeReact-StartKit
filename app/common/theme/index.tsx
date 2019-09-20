bf6ade6d893decb7f493e05a68a0b4ec4b2fcdaf
GenesisSam <samuel940401@gmail.com>
2019-09-20 23:41:16 +0900

    Type safe for styled-components theme

--- app/common/theme/index.tsx
+++ app/common/theme/index.tsx
@@ -1,8 +1,8 @@
-const basicTheme = {
-	primary: "#3B3E45",
-	darker: "#15181e",
-	lighter: "#656870",
-	buttonColor: "#63DBD6",
+const basicTheme: ITheme = {
+  primary: "#3B3E45",
+  darker: "#15181e",
+  lighter: "#656870",
+  buttonColor: "#63DBD6"
 };
 
 export default basicTheme;
--- typings/common.d.ts
+++ typings/common.d.ts
@@ -1,1 +1,8 @@
 type SUPPORT_LOCALE = "ko" | "en";
+
+interface ITheme {
+  primary: string;
+  darker: string;
+  lighter: string;
+  buttonColor: string;
+}
