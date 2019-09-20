e098ea0bb226934b37b455cdbbf33219b9577644
GenesisSam <samuel940401@gmail.com>
2019-09-20 23:42:58 +0900

    Fix typesafe redux action

--- app/actions/app.tsx
+++ app/actions/app.tsx
@@ -1,17 +1,21 @@
 import { APP_TYPES } from "./types";
 import { ActionThunk, ActionExtractor } from "common/helpers/reduxHelper";
 
+function createAction<T extends { type: APP_TYPES }>(d: T): T {
+  return d;
+}
+
 export const ActionCreator = {
-	changeCurrentUser: (userData: Model.IRawUser | null) => ({
-		type: APP_TYPES.CHANGE_CURRENT_USER,
-		payload: { userData },
-	}),
+  changeCurrentUser: (userData: Model.IRawUser | null) => ({
+    type: APP_TYPES.CHANGE_CURRENT_USER,
+    payload: { userData }
+  })
 };
 
 export type ActionTypes = ActionExtractor<typeof ActionCreator>;
 
 export const changeCurrentUser = (params: { userData: Model.IRawUser | null }): ActionThunk => {
-	return dispatch => {
-		dispatch(ActionCreator.changeCurrentUser(params.userData));
-	};
+  return dispatch => {
+    dispatch(ActionCreator.changeCurrentUser(params.userData));
+  };
 };
